ba7056a1638e4ed03920150b37aefb4b2222698c
srikanthpragada <srikanthpragada@gmail.com>
2023-03-31 18:56:24 +0530

    31-MAR

---
+++ largest_of_3_v2.c
@@ -0,0 +1,23 @@
+// Print largest of 3 numbers with nested ifs
+// Date : 31-mar-2023
+
+#include <stdio.h>
+
+void main()
+{
+ int a,b,c;
+
+    printf("Enter 3 numbers :");
+    scanf("%d%d%d", &a, &b, &c);
+
+    if(a > b)
+        if(a > c)
+           printf("%d",a);
+        else
+            printf("%d", c);
+    else
+        if(b > c)
+            printf("%d", b);
+        else
+            printf("%d", c);
+}
---
+++ month_days.c
@@ -0,0 +1,27 @@
+// Take month number and display no. of days
+// Date : 31-mar-2023
+
+#include <stdio.h>
+
+void main()
+{
+ int month, year;
+
+    printf("Enter month [1-12] :");
+    scanf("%d", &month);
+
+    if(month == 2)
+    {
+       printf("Enter year : ");
+       scanf("%d",&year);
+       if(year % 4 == 0)  // leap year
+           printf("29");
+       else
+           printf("28");
+    }
+    else
+        if(month == 4 || month == 6 || month == 9 || month == 11)
+           printf("30");
+        else
+           printf("31");
+}
